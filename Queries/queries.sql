3bb12e4f8251d433048f8ffcc33b8b6ffee43fbb
Lois K <amulya.lois@gmail.com>
2022-09-20 12:17:29 -0500

    Added retirement info

---
+++ Queries/queries.sql
@@ -0,0 +1,40 @@
+SELECT * FROM departments
+
+SELECT * FROM employees where birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT * FROM dept_manager
+
+SELECT * FROM dept_employee where emp_no = 10004
+
+SELECT * FROM salaries
+
+SELECT * FROM titles
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Import the select results into another table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+select * from retirement_info
